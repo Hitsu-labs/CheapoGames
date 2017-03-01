2b9210a7165045e01f6cfe69c0842df2e2b337ea
Shadowbroker <zakie95hinds@gmail.com>
2017-03-01 11:44:57 -0500

    Sorting out files, again

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -5,7 +5,7 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
+$file = fopen("logout.txt","w");
 
 $request = array();
 $request['type'] = "login";
@@ -13,6 +13,8 @@ $request['username'] = $argv[1];
 $request['password'] = $argv[2];
 $request['message'] = "HI";
 $response = $client->send_request($request);
+echo fwrite($file,$request);
+fclose($file);
 //$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
