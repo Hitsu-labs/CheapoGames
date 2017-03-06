e59dcc55a5670a1b811473212e677f68aee0b62b
Shadowbroker <zakie95hinds@gmail.com>
2017-03-06 11:30:47 -0500

    Loggin functions completed, Didn't make any funtions to counter fuzzing. Looking for workability than security right now. Awaiting front end. Also need to figure out how to bridge this through RabbitMQ.

--- PHPcodes/logginfunctions.php
+++ PHPcodes/logginfunctions.php
@@ -21,11 +21,20 @@ if(Userduplicate($user,$email,$dbh)){
 function loggin ($user,$pwd,$dbh)
 {
 
-	$sqliquery = "SELECT 'username','pwd' FROM 'Account` WHERE 'username'= '$user' ";
-	$templogindata = mysqli_query($dbh,$sqliquery);
+	$sqliquery = "SELECT 'accountid','username','pwd' FROM 'Account` WHERE 'username'= '$user' ";
+	$templogindata = mysqli_query($dbh,$sqliquery) or die(mysqli_error());
+	$row=mysqli_fetch_assoc($templogindata);
+	if(password_verify($pwd,$row["pwd"])){
 // After logging in (which I'll do at some point). We start the session and assign variables to the session. From there it should be easy....right? erm...
 	session_start();
-
+	$_SESSION["username"] = $user;
+	$_SESSION["accountid"] = $row["accountid"];
+	return true;
+	}
+	else
+	{
+	return FALSE;
+	}
 }
 //Checks if the user has a dupe or not
 function Userduplicate($user,$email,$dbh)
