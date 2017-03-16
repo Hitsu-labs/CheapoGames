db92dfebc416f243701db022da090adaae3af9bd
Shadowbroker <zakie95hinds@gmail.com>
2017-03-16 12:16:57 -0400

    We have the ground work for server to server communications WE DID ITgit statusgit status

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,14 +3,12 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('login.php.inc');
 
 function doLogin($username,$password)
 {
     // lookup username in databas
     // check password
-    $login = new loginDB();
-    return $login->validateLogin($username,$password);
+    return true;
     //return false if not valid
 }
 
@@ -25,6 +23,7 @@ function requestProcessor($request)
   switch ($request['type'])
   {
     case "login":
+      print($request['swag']);	
       return doLogin($request['username'],$request['password']);
     case "validate_session":
       return doValidate($request['sessionId']);
