f791eca67d01fb9cc14833dd57af81751c32095f
Min Kim <mk557@njit.edu>
2017-03-16 11:40:13 -0700

    Update

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,8 +3,13 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+<<<<<<< HEAD
+//include("logginfunctions.php");
+//include("mysqlloggininfo");
+=======
 include("logginfunctions.php");
 include("mysqlloggininfo");
+>>>>>>> 5bdac01306432697f69a8da76e8bd72e64b88653
 ($dbh = mysqli_connect($hostname, $username, $password, $database)) or die ("SQL connection rejected, try again");
 function doLogin($username,$password)
 {
@@ -12,10 +17,15 @@ function doLogin($username,$password)
     // check password
     return true;
     //return false if not valid
+    
+   if(rcode=0) {
+	$string = "Response 
+   }
 }
 
 function requestProcessor($request)
 {
+  $rcode;
   echo "received request".PHP_EOL;
   var_dump($request);
   if(!isset($request['type']))
@@ -30,12 +40,36 @@ function requestProcessor($request)
     case "validate_session":
       return doValidate($request['sessionId']);
     case "loggingin":
+<<<<<<< HEAD
+      if(loggin($request['username'],$request['password'],$dbh))
+	{
+	$rcode=0;
+	return $rcode;
+	}
+else 
+{
+	$rcode=1
+}
+/*    case "register":
+      if(registration($user,$email,$pwd,$dbh))
+{
+	$rcode=2;
+	return $rcode;
+}
+else
+{
+	$rcode=3;
+}*/
+=======
       return loggin($user,$pwd,$dbh);
     case "register":
       return registration($user,$email,$pwd,$dbh);
+>>>>>>> 5bdac01306432697f69a8da76e8bd72e64b88653
   }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+  return array($rcode);
 }
+
+
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
