6aa632e6ad1ba8da125e4a1a8fc2ec519d9895e7
Hitsugata <zakie95hinds@gmail.com>
2017-03-20 15:13:56 -0400

    repushing Carl's database setup

--- Games.sql
+++ Games.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 01, 2017 at 01:58 PM
+-- Generation Time: Mar 20, 2017 at 03:06 PM
 -- Server version: 5.7.17-0ubuntu0.16.04.1
 -- PHP Version: 5.6.30-5+deb.sury.org~xenial+2
 
@@ -23,6 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Account`
+--
+
+CREATE TABLE `Account` (
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(200) NOT NULL,
+  `ID` int(255) NOT NULL,
+  `Email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Games`
 --
 
@@ -35,9 +48,26 @@ CREATE TABLE `Games` (
   `Price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Wishlist`
+--
+
+CREATE TABLE `Wishlist` (
+  `AccountID` int(255) NOT NULL,
+  `GameID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Account`
+--
+ALTER TABLE `Account`
+  ADD PRIMARY KEY (`ID`,`Username`);
 
 --
 -- Indexes for table `Games`
@@ -46,14 +76,37 @@ ALTER TABLE `Games`
   ADD PRIMARY KEY (`GameID`,`Title`);
 
 --
+-- Indexes for table `Wishlist`
+--
+ALTER TABLE `Wishlist`
+  ADD KEY `AccountID` (`AccountID`),
+  ADD KEY `GameID` (`GameID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `Account`
+--
+ALTER TABLE `Account`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `Games`
 --
 ALTER TABLE `Games`
   MODIFY `GameID` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Wishlist`
+--
+ALTER TABLE `Wishlist`
+  ADD CONSTRAINT `Wishlist_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `Account` (`ID`),
+  ADD CONSTRAINT `Wishlist_ibfk_2` FOREIGN KEY (`GameID`) REFERENCES `Games` (`GameID`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
