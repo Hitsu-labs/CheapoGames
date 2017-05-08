7532e1d097c0be0c36cf5047bace7e7f6b2f6929
chillado <cwh5@njit.edu>
2017-05-08 19:10:07 -0400

    Deploy Systems

---
+++ dmzdeploy.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","deployServer");
+$request = array();
+$request['type'] = "dmz";
+$request['function'] = $argv[1];
+shell_exec('bashdeploy/sendman.sh'.$argv[1].$argv[2]); 
+
+$response = $client->send_request($request);
+?>
