c653c8b8639684081f3b6296955dab0bea9b34c6
Min Kim <mk557@njit.edu>
2017-05-08 12:47:27 -0400

    Search push last

--- searchengine/search.php
+++ searchengine/search.php
@@ -1,21 +1,27 @@
 <input type="button" onclick="location.href='http://127.0.0.1/searchengine';" value="Search for another game"/><br><br>
 
      <html>
+	<section class="webdesigntuts-workshop"><br>
+	<form action="search.php" method="GET">		    
+		<input type="text" placeholder="What are you looking for?" name = "search">		    	
+		<button>Search</button>
+	</form>
+</section>
      <head></head>
      <body>
-
+</html>
 <?php
-echo '<body style="background-color:#151515">';
+//echo '<body style="background-color:#151515">';
 $button = $_GET ['submit'];
 $search = $_GET ['search']; 
 
 if(strlen($search)<=1)
-echo "<font color='white'>Search term too short</font>";
+echo "Search term too short";
 else{
-echo "<font color='white'>You searched for <u><b>$search</b></u> <hr size='1'></br></font>";
+echo "<p style = 'font-family: Open Sans, sans-serif; font-size:28px; font-weight:300; letter-spacing: 1px; line-height:50px; text-align:center; display:block; text-transform: uppercase; padding-bottom:-10px'>SEARCH RESULTS FOR: <u><b>$search</b></u></p> <hr size='1';></br>";
 mysql_connect("localhost","root","kim020995");
 mysql_select_db("gamestop");
-    
+
 $search_exploded = explode (" ", $search);
  
 $x = "";
@@ -37,18 +43,18 @@ $run = mysql_query($constructs);
 $foundnum = mysql_num_rows($run);
     
 if ($foundnum==0)
-echo "<font color='white'>Sorry, there are no matching result for <b>$search</b>.</br></br>
-Here are some helpful tips to help your search!</font><br><br><font color='white'>
+echo "Sorry, there are no matching result for <b>$search</b>.</br></br>
+Here are some helpful tips to help your search!<br><br>
 1. 
 Try more general words. For example: If you want to search 'The Legend of Zelda Breath of Wild Special Edition'
 then use general keyword like 'Zelda'</br>2. Try different words with similar
- meaning</br>3. Please check your spelling</font>";
+ meaning</br>3. Please check your spelling";
 else
 { 
   
-echo "<font color='white'>$foundnum result(s) found!<p></font>";
+echo "<p style='padding-bottom:0px; font-family: Open Sans, sans-serif; font-size: 18px; font-weight: 500; letter-spacing: 1px; line-height:0px; padding-bottom:30px; text-align:center; display: block;'>$foundnum result(s) found!</p>";
   
-$per_page = 1;
+$per_page = 5;
 $start = isset($_GET['start']) ? $_GET['start']: '';
 $max_pages = ceil($foundnum / $per_page);
 if(!$start)
@@ -60,14 +66,17 @@ while($runrows = mysql_fetch_assoc($getquery))
 $title = $runrows ['title'];
 $price = $runrows ['price'];
 $url = $runrows ['url'];
-   
-echo "<font color='white'>
-Game Title: <a href='$url'><b>$title</b></a><br>
-Price: $price<br></font>
-";
+$image = $runrows ['image'];
+
+
+echo "<a href='$url'><img src ='".$image."' height='50' width = '70' align = left></a>";
+//echo "<a href=$url><b>$title</b></a>";
+echo "<a href=$url><p style = 'font-family: Open Sans, sans-serif; font-size:15px; font-weight:300; letter-spacing:1px; display:block; align:left; height:10px;'>".$title."</p></a>";
+echo "<p style = 'font-family: Open Sans, sans-serif; font-size:15px; font-weight:600; display:block; padding-bottom:20px'>".$price."</p>";
+//echo "<p style = ''>".$price."</p>";
     
 }
-  
+
 //Pagination Starts
 echo "<center>";
   
@@ -157,7 +166,7 @@ $i = $i + $per_page;
           
 //next button
 if (!($start >=$foundnum-$per_page))
-echo "<font color='white'><a href='search.php?search=$search&submit=Search+source+code&start=$next'>Next</font></a> ";    
+echo "<a href='search.php?search=$search&submit=Search+source+code&start=$next'>Next</a> ";    
 }   
 echo "</center>";
 } 
