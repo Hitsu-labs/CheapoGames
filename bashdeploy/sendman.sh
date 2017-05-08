37cc54bb8b492f403031c96e4048a650ff85409a
Shadowbroker <zakie95hinds@gmail.com>
2017-05-08 16:42:40 -0400

    Sending packages with version control

--- bashdeploy/sendman.sh
+++ bashdeploy/sendman.sh
@@ -7,19 +7,19 @@ cd ..
 
 case $1 in
 "deploy")
-	scp fulldeploy.tar.gz shadowbroker@192.168.1.4:
+	scp fulldeploy$2.tar.gz vman shadowbroker@192.168.1.4:
 	scp vman shadowbroker@192.168.1.4
 	;;
 "front")
-	scp fulldeploy.tar.gz shadowbroker@192.168.1.5:
+	scp fulldeploy$2.tar.gz vman shadowbroker@192.168.1.5:
 	scp vman shadowbroker@192.168.1.5
 	;;
 "backend")
-	scp fulldeploy.tar.gz chilado@192.168.1.7:
+	scp fulldeploy$2.tar.gz vman chilado@192.168.1.7:
 	scp vman chilado@192.168.1.7
 	;;
 "dmz")
-	scp fulldeploy.tar.gz mk\5\5\7@192.168.1.6:
+	scp fulldeploy$2.tar.gz vman mk\5\5\7@192.168.1.6:
 	scp vman mk\5\5\7@192.168.1.6
 	;;
 esac
